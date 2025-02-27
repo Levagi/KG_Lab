c1c0eab484208d43b3a2d9335387ec64895345f2
Levagi <artemagafonov2005@mail.ru>
2025-02-27 17:15:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import numpy as np
 from PIL import Image, ImageOps
 import math
+from random import randint
 
 
 img_mat = np.zeros ((2000, 2000, 3), dtype = np.uint8)
-
+zbuff = np.full((2000, 2000), 100000, dtype=np.float32)
 def draw_line(img_mat, x0, y0, x1, y1, color):
     count = math.sqrt((x0-x1)**2 + (y0 - y1) ** 2)
     step = 1.0/count
@@ -106,22 +107,47 @@ def bresenham_line(img_mat, x0, y0, x1, y1, color):
             derror -= 2 * (x1 - x0)
             y += y_update
 
+def bar_cor(x0, x1, x2, y0, y1, y2, x, y):
+    lambda0 = ((x - x2) * (y1 - y2) - (x1 - x2) * (y - y2)) / ((x0 - x2) * (y1 - y2) - (x1 - x2) * (y0 - y2))
+    lambda1 =  ((x0 - x2) * (y - y2) - (x - x2) * (y0 - y2)) / ((x0 - x2) * (y1 - y2) - (x1 - x2) * (y0 - y2))
+    lambda2 = 1 - lambda1 - lambda0
+    return lambda0, lambda1, lambda2
 
-#for i in range(13):
-    #x0 = 100
-    #y0 = 100
-    #x1 = int(100 + 95*math.cos(i*2*math.pi/13))
-    #y1 = int(100 + 95*math.sin(i*2*math.pi/13))
-    #draw_line(img_mat, x0, y0, x1, y1, 255)
-    #x_loop_line(img_mat, x0, y0, x1, y1, 255)
-    #x_loop_line_hotfix_v1(img_mat, x0, y0, x1, y1, 255)
-    #x_loop_line_hotfix_v2(img_mat, x0, y0, x1, y1, 255)
-    #x_loop_line_v2(img_mat, x0, y0, x1, y1, 255)
-    #x_loop_line_v2_no_y_calc(img_mat, x0, y0, x1, y1, 255)
-    #bresenham_line(img_mat, x0, y0, x1, y1, 255)
+def draw_triangle(x0, y0, z0, x1, y1, z1, x2, y2, z2, zbuff, img_mat, color):
+    xmin = math.floor(min(x0 ,x1, x2))
+    if (xmin < 0): xmin = 0
+    xmax = math.ceil(max(x0, x1, x2))
+    if (xmax > img_mat.shape[0]): xmax = img_mat.shape[0]
+    ymin = math.floor(min(y0, y1, y2))
+    if (ymin < 0): ymin = 0
+    ymax = math.ceil(max(y0, y1, y2))
+    if (ymax > img_mat.shape[1]): ymax = img_mat.shape[1]
+    for x in range(xmin, xmax):
+        for y in range(ymin, ymax):
+            barcord = (bar_cor(x0, x1, x2, y0, y1, y2, x, y))
+            if barcord[0] > 0 and barcord[1] > 0 and barcord[2] > 0:
+                z = barcord[0]*z0 + barcord[1]*z1 + barcord[2] * z2
+                if zbuff[y,x] > z:
+                    img_mat[y, x] = color
+                    zbuff[y,x] = z
 
-#img = Image.fromarray(img_mat, mode = 'RGB')
-#img.save('img.png')
+
+
+# for i in range(13):
+#     x0 = 100
+#     y0 = 100
+#     x1 = int(100 + 95*math.cos(i*2*math.pi/13))
+#     y1 = int(100 + 95*math.sin(i*2*math.pi/13))
+#     draw_line(img_mat, x0, y0, x1, y1, 255)
+#     x_loop_line(img_mat, x0, y0, x1, y1, 255)
+#     x_loop_line_hotfix_v1(img_mat, x0, y0, x1, y1, 255)
+#     x_loop_line_hotfix_v2(img_mat, x0, y0, x1, y1, 255)
+#     x_loop_line_v2(img_mat, x0, y0, x1, y1, 255)
+#     x_loop_line_v2_no_y_calc(img_mat, x0, y0, x1, y1, 255)
+#     bresenham_line(img_mat, x0, y0, x1, y1, 255)
+#
+# img = Image.fromarray(img_mat, mode = 'RGB')
+# img.save('img.png')
 
 f = open ('model_1.obj')
 v = []
@@ -135,23 +161,24 @@ for s in f:
 
 
 
-for vertex in v:
-    img_mat[int(10000*vertex[1]) + 1000, int(10000*vertex[0])+1000] = (139, 0, 255)
-
+# for vertex in v:
+#     img_mat[int(10000*vertex[1]) + 1000, int(10000*vertex[0])+1000] = (139, 0, 255)
 for face in vec:
     x0 = 10000*v[face[0] - 1][0] + 1000
     y0 = 10000*v[face[0] - 1][1] + 1000
+    z0 = 10000*v[face[0] - 1][2] + 1000
     x1 = 10000*v[face[1] - 1][0] + 1000
     y1 = 10000*v[face[1] - 1][1] + 1000
+    z1 = 10000*v[face[1] - 1][2] + 1000
     x2 = 10000*v[face[2] - 1][0] + 1000
     y2 = 10000*v[face[2] - 1][1] + 1000
-    bresenham_line(img_mat, int(x0), int(y0), int(x1), int(y1), (139,0,255))
-    bresenham_line(img_mat, int(x1), int(y1), int(x2), int(y2), (139,0,255))
-    bresenham_line(img_mat, int(x0), int(y0), int(x2), int(y2), (139, 0, 255))
-
+    z2 = 10000*v[face[2] - 1][2] + 1000
+    # bresenham_line(img_mat, int(x0), int(y0), int(x1), int(y1), (139,0,255))
+    # bresenham_line(img_mat, int(x1), int(y1), int(x2), int(y2), (139,0,255))
+    # bresenham_line(img_mat, int(x0), int(y0), int(x2), int(y2), (139, 0, 255))
+    n = np.cross([x1 - x2, y1 - y2, z1 - z2], [x1 - x0, y1 - y0, z1 - z0])
+    cosin = np.dot(n, [0, 0, 1]) / (np.linalg.norm(n) * np.linalg.norm([0, 0, 1]))
+    if cosin < 0: draw_triangle(x0, y0, z0, x1, y1, z1, x2, y2, z2, zbuff, img_mat, (cosin*(-255), 0, 0))
 img = Image.fromarray(img_mat, mode = 'RGB')
 img = ImageOps.flip(img)
 img.save('img.png')
-
-
-
